5e7a36329d60d880935a7eae23a302bfa5c2d33d
codergaint <71995368+codergaint@users.noreply.github.com>
2025-07-12 09:44:51 +0600

    Update app.php

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Retail Nova'),
+    'name' => env('APP_NAME', 'RetailNova'),
 
     /*
     |--------------------------------------------------------------------------
